68551f9204203744abc0e03a2347d3e947c04686
dozken <dosmukhamed@gmail.com>
2024-07-06 03:07:49 +0500

    feat: undotree + git worktree

---
+++ lua/plugins/git.lua
@@ -0,0 +1,30 @@
+return {
+
+  {
+    "mbbill/undotree",
+    keys = { { "<leader>fu", "<cmd>UndotreeToggle<cr>", mode = "n", desc = "File Undotree" } },
+  },
+
+  {
+    "ThePrimeagen/git-worktree.nvim",
+    keys = {
+      {
+        "<leader>gw",
+        "<CMD>lua require('telescope').extensions.git_worktree.git_worktrees()<CR>",
+        desc = "[G]it [W]orktree",
+      },
+      {
+        "<leader>gW",
+        "<CMD>lua require('telescope').extensions.git_worktree.create_git_worktree()<CR>",
+        desc = "Git Worktree Create",
+      },
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      require("telescope").load_extension("git_worktree")
+    end,
+  },
+}
