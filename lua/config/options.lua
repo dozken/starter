bad29b78760528e0c9f0392d8e3d28a8b5dbf632
dozken <dosmukhamed@gmail.com>
2024-07-06 03:06:36 +0500

    feat: more options for save

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Wa to wa
+vim.api.nvim_create_user_command("Wa", "wa", {})
+vim.api.nvim_create_user_command("WA", "wa", {})
+vim.api.nvim_create_user_command("W", "w", {})
