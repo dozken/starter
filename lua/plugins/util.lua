84d2c4fb89382fc835f4fa385cfc0b1019133fe5
dozken <dosmukhamed@gmail.com>
2024-07-06 03:08:06 +0500

    feat: vim tmux navigator
    s

---
+++ lua/plugins/util.lua
@@ -0,0 +1,11 @@
+return {
+  { "christoomey/vim-tmux-navigator", keys = { "<C-h>", "<C-j>", "<C-k>", "<C-l>" } },
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   dependencies = { "hrsh7th/cmp-emoji" },
+  --   ---@param opts cmp.ConfigSchema
+  --   opts = function(_, opts)
+  --     table.insert(opts.sources, { name = "emoji" })
+  --   end,
+  -- },
+}
