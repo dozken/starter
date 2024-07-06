4ed33512a7a14c3c7007cc5ca28763a0421e7744
dozken <dosmukhamed@gmail.com>
2024-07-06 11:17:24 +0500

    feat: keymaps
    keymap for yanking

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll & center down" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll & center up" })
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to clipboard" })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Yank line to clipboard" })
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Replace with clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete to void" })
