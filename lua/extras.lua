d940c77cb9df682c3725e928a74149f164b956b1
dozken <dosmukhamed@gmail.com>
2024-07-09 14:13:12 +0500

    feat: default extras
    remvoe codeium

---
+++ lua/extras.lua
@@ -0,0 +1,26 @@
+return {
+  { import = "lazyvim.plugins.extras.dap.core" },
+
+  { import = "lazyvim.plugins.extras.test.core" },
+
+  { import = "lazyvim.plugins.extras.editor.harpoon2" },
+  { import = "lazyvim.plugins.extras.editor.refactoring" },
+
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.docker" },
+  { import = "lazyvim.plugins.extras.lang.git" },
+  { import = "lazyvim.plugins.extras.lang.go" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.markdown" },
+  { import = "lazyvim.plugins.extras.lang.sql" },
+  { import = "lazyvim.plugins.extras.lang.tailwind" },
+  { import = "lazyvim.plugins.extras.lang.toml" },
+  { import = "lazyvim.plugins.extras.lang.yaml" },
+
+  { import = "lazyvim.plugins.extras.lsp.none-ls" },
+
+  { import = "lazyvim.plugins.extras.ui.treesitter-context" },
+
+  { import = "lazyvim.plugins.extras.util.dot" },
+  { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+}
